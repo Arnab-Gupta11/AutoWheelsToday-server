86634894127bb0f455a3c656af1b1df89c34ccb0
Arnab Gupta <arnabgupta1111@gmail.com>
2023-10-19 14:00:10 +0600

    add brands collection in database

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const brandColection = client.db("BrandShopDB").collection("brands");
+
+    app.get("/brands", async (req, res) => {
+      const cursor = brandColection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
